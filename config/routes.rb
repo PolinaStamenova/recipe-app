964e6fb10798100c9497fcd3e3ae9e314602d2c4
Akasha <Akasha.Rojee@gmail.com>
2021-11-15 19:49:08 +0400

    Create routes for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'recipes/index'
-  get 'recipes/create'
-  get 'recipes/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'foods#index'
   resources :foods, only: [:index, :create, :destroy]
+
+  resources :recipes, only: [:index, :create, :destroy]
 end
