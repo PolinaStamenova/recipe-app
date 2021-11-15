d29f8b6e1c65299d10c188abd4ab1cbdb959f68d
Akasha <Akasha.Rojee@gmail.com>
2021-11-15 22:43:36 +0400

    Add recipe create

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,7 +7,13 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
   end
 
-  def create; end
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    if @recipe.save
+      flash[:notice] = 'Recipe successfully added!'
+    end
+    redirect_to recipes_path
+  end
 
   def destroy; end
 end
