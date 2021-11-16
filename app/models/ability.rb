bb27cf2a7cb39c11bbeca19c9bc8840af3878678
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-16 11:12:42 +0200

    Update Users tabl: Add role column

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,19 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+      user ||= User.new # guest user (not logged in)
+      if user.role == 'admin'
+        can :manage, :all
+      else
+        can :read, :all
+        can :destroy, Food do |food|
+          food.user.id == user.id
+        end
+
+        can :destroy, Recipe do |recipe|
+          food.user.id == user.id
+        end
+      end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
