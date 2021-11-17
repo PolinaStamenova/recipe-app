dcfeebd819f1fc241d166ddf6575d0a687bad9c4
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-17 21:56:10 +0200

    Add eidt method to recipe_foods so user can edit the choosen foods

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -4,16 +4,34 @@ class RecipeFoodsController < ApplicationController
     @recipe_food = @recipe.recipe_foods.new
   end
 
+  def edit
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
   def create
     @recipe = Recipe.find(params[:recipe_id])
-    @recipe_foods = @recipe.recipe_foods.create(recipe_foods_params.merge(recipe_id: @recipe.id))
+    @recipe_foods = @recipe.recipe_foods.create(recipe_foods_params)
     if @recipe_foods.save
       flash[:notice] = 'Recipe food successfully added!'
-      redirect_back(fallback_location: root_path)
     else
       flash[:notice] = 'Food was not added!'
-      redirect_back(fallback_location: root_path)
     end
+    redirect_to @recipe
+  end
+
+  def update
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_foods = @recipe.recipe_foods.update(recipe_foods_params)
+    flash[:notice] = 'Food was successfully updated!'
+    redirect_to @recipe
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe_food.destroy
+    flash[:notice] = 'Food was successfully removed from the recipe!'
+    redirect_to @recipe
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'foods#index'
   resources :foods, only: [:index, :create, :destroy, :new]
 
+  # resources :recipe_foods, only: [:edit, :update]
+
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
-    resources :recipe_foods, only: [:new, :create]
+    resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
   end
+
 
   get 'public_recipes', to: 'recipes#public_recipes'
 end
