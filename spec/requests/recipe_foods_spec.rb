9aec072d49968f8dbb6591f98e749563f95b0e99
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-18 21:56:35 +0200

    Fix linters

--- spec/requests/recipe_foods_spec.rb
+++ spec/requests/recipe_foods_spec.rb
@@ -1,17 +1,20 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe 'RecipeFoods', type: :request do
   include Devise::Test::IntegrationHelpers
 
   let(:user) { User.create(name: 'Akasha', email: 'akasha@mail.com', password: 'password') }
 
-  let(:recipe) { Recipe.create(name: "Chicken filet", description: "Cook it", preparation_time: "1 hour", cooking_time: "1 hour", public: true, user: user )}
+  let(:recipe) do
+    Recipe.create(name: 'Chicken filet', description: 'Cook it', preparation_time: '1 hour', cooking_time: '1 hour',
+                  public: true, user: user)
+  end
 
-  let(:recipe_food) { RecipeFood.create(recipe_id: recipe.id)}
+  let(:recipe_food) { RecipeFood.create(recipe_id: recipe.id) }
   describe 'GET /new' do
     before do
       sign_in user
-      get new_recipe_recipe_food_path(recipe_id: recipe.id )
+      get new_recipe_recipe_food_path(recipe_id: recipe.id)
     end
 
     it 'returns http ok' do
